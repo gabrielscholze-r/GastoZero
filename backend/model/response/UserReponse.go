7b3feb26cf44e422d8fb0dfdc8aa4e0561a84517
gabrielscholze-r <gabrielscholze24@gmail.com>
2025-04-11 20:38:57 -0300

    🔧

--- backend/model/response/UserReponse.go
+++ backend/model/response/UserReponse.go
@@ -3,7 +3,7 @@ package response
 import "time"
 
 type UserResponse struct {
-	ID          int32     `json:"id"`
+	ID          int       `json:"id"`
 	Name        string    `json:"name"`
 	Email       string    `json:"email"`
 	CreatedDate time.Time `json:"created_date"`
